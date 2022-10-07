c47014c5fbfffb99aa252ea865ced1ab10a71e33
alcid <alcides_thiago@hotmail.com>
2022-10-07 10:52:35 -0300

    Initial commit

---
+++ src/devSuperior/application/Program.java
@@ -0,0 +1,12 @@
+package devSuperior.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.ENGLISH);
+        Scanner sc = new Scanner(System.in);
+    }
+}
